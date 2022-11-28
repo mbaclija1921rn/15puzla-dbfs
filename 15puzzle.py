38a3fb0010c61dec430118692d1ad519e73131d8
mbaclija1921rn <116192274+mbaclija1921rn@users.noreply.github.com>
2022-11-28 12:15:54 +0100

    Add files via upload
    numpy, cuvanje zero_pos

--- 15puzzle.py
+++ 15puzzle.py
@@ -1,5 +1,7 @@
 from random import choice
 from collections import deque
+import numpy as np
+import time
 
 # bfs sa obe strane da bi se minimizovalo rasireno grananje
 
@@ -15,10 +17,8 @@ def legit(y, x, m, n):
 
 
 def find0(matrix):
-    for i, row in enumerate(matrix):
-        for j, el in enumerate(row):
-            if el == 0:
-                return (i, j)
+    a = np.where(matrix == 0)
+    return (a[0][0], a[1][0])
 
 
 def make_end_pos(m, n):
@@ -30,7 +30,7 @@ def make_end_pos(m, n):
             lst.append(num)
         end_pos.append(lst)
     end_pos[m - 1][n - 1] = 0
-    return end_pos
+    return np.array(end_pos, np.int8)
 
 
 def make_random_start_pos(m, n, shuffle_count=1000):
@@ -43,59 +43,47 @@ def make_random_start_pos(m, n, shuffle_count=1000):
         if legit(n0y, n0x, m, n):
             pos[c0y][c0x], pos[n0y][n0x] = pos[n0y][n0x], pos[c0y][c0x]
             c0y, c0x = n0y, n0x
-    return pos
+    return np.array(pos, np.int8)
 
 
 class State:
-    def __init__(self, parent, matrix, last_move):
+    def __init__(self, parent, matrix, zero_pos, last_move):
         self.parent = parent
         self.matrix = matrix
+        self.zero_pos = zero_pos
         self.last_move = last_move
 
-    def shallow_copy_matrix(self):
-        n_mat = []
-        for row in self.matrix:
-            n_mat.append(row.copy())
-        return n_mat
-
     def create_children(self):
-        m = len(self.matrix)
-        n = len(self.matrix[0])
-        c0y, c0x = find0(self.matrix)
+        m, n = self.matrix.shape
+        c0y, c0x = self.zero_pos
         for dy, dx, dname in directions:
             n0y = c0y + dy
             n0x = c0x + dx
             if legit(n0y, n0x, m, n):
-                n_mat = self.shallow_copy_matrix()
+                n_mat = np.copy(self.matrix)
                 n_mat[n0y][n0x], n_mat[c0y][c0x] = n_mat[c0y][c0x], n_mat[n0y][n0x]
-                yield State(self, n_mat, dname)
+                yield State(self, n_mat, (n0y, n0x), dname)
 
     def print_matrix(self):
         print("-" * 50)
         for row in self.matrix:
-            print("\t".join(map(str, row)))
+            print("\t".join(map(str, row.tolist())))
 
     def __hash__(self):
-        tmp = []
-        for row in self.matrix:
-            tmp.append(tuple(row))
-        return hash(tuple(tmp))
+        return hash(self.matrix.data.tobytes())
 
     def __eq__(self, other):
-        if other is None:
-            return False
-        for a, b in zip(self.matrix, other.matrix):
-            if a != b:
-                return False
-        return True
+        return np.array_equal(self.matrix, other.matrix)
 
 
-def solve(m, n, start_pos):
+def solve(start_pos):
+    m, n = start_pos.shape
     end_pos = make_end_pos(m, n)
-    start_state = State(None, start_pos, "_start_")
+    start_state = State(None, start_pos, find0(start_pos), "_start_")
     print("Starting position:")
     start_state.print_matrix()
-    end_state = State(None, end_pos, "_end_")
+    end_state = State(None, end_pos, find0(end_pos), "_end_")
+    end_state.print_matrix()
     last_level1 = [start_state]
     last_level2 = [end_state]
     record1 = dict()
@@ -140,14 +128,14 @@ def solve(m, n, start_pos):
 
     lst1 = []
     curr = record1[intersection]
-    while curr != None:
+    while curr is not None:
         lst1.append(curr)
         curr = curr.parent
     lst1 = lst1[::-1]
 
     lst2 = []
     curr = record2[intersection]
-    while curr != None:
+    while curr is not None:
         lst2.append(curr)
         curr = curr.parent
 
@@ -170,6 +158,7 @@ def solve(m, n, start_pos):
 
 if __name__ == "__main__":
     m, n = map(int, input("Enter dimension (M N): ").split())
+
     op = 0
     while op < 1 or op > 2:
         op = int(input("Auto generate matrix (1) or input a matrix (2): "))
@@ -177,9 +166,13 @@ if __name__ == "__main__":
             start_pos = make_random_start_pos(m, n, 10000)
             # 10000, ubije program ili kompijuter posle 10M shallow kopija za 4x4
         elif op == 2:
-            start_pos = [
-                list(map(int, input(f"Row {i+1}: ").split())) for i in range(m)
-            ]
+            start_pos = np.array(
+                [list(map(int, input(f"Row {i+1}: ").split())) for i in range(m)],
+                np.int8,
+            )
         else:
             print("Wrong input")
-    solve(m, n, start_pos)
+    start_t = time.time()
+    solve(start_pos)
+    end_t = time.time()
+    print(f"Finished after {end_t - start_t}s")
